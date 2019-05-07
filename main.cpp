7fba00a2abf76bff8f6517cf73524aceeff995b0
Mauro Delazeri <maurodelazeri@gmail.com>
2019-05-06 20:25:34 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
     KafkaProducer* m_pProducerObject;
     m_pProducerObject = new KafkaProducer();
     m_pProducerObject->PutBrokers("xx.xx.xx.xx:9092");
-    m_pProducerObject->PutTopics("mauro");
+    m_pProducerObject->PutTopics({"mauro","test"});
     m_pProducerObject->initKafka();
 
     std::string xxx = "Hello Kafka!";
